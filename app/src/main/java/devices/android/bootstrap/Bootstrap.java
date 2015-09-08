8cc0295e722323880005bd09fde2706cea30857c
unknown <anand.sadu@imaginea.com>
2015-09-08 14:39:56 +0530

    added netty server

--- app/src/main/java/devices/android/bootstrap/Bootstrap.java
+++ app/src/main/java/devices/android/bootstrap/Bootstrap.java
@@ -3,8 +3,9 @@ package devices.android.bootstrap;
 import android.support.test.uiautomator.UiDevice;
 import android.test.InstrumentationTestCase;
 
-import devices.android.bootstrap.server.v2_2.HttpServer;
-
+import devices.android.bootstrap.server.netty.HttpServer;
+//import devices.android.bootstrap.server.v2_2.HttpServer;
+//import devices.android.bootstrap.server.HttpServer ;
 public class Bootstrap extends InstrumentationTestCase {
 
     private static UiDevice device;
